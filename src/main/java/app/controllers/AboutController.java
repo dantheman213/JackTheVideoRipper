316f15c02c882d4034744936cf517e9e73140d79
Daniel Gillespie <daniel.gillespie0949@gmail.com>
2018-07-03 17:05:32 -0700

    setup some new buttons. changed layout. added about view

---
+++ src/main/java/app/controllers/AboutController.java
@@ -0,0 +1,14 @@
+package app.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AboutController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
--- src/main/java/app/controllers/EncoderController.java
+++ src/main/java/app/controllers/EncoderController.java
@@ -1,6 +1,18 @@
 package app.controllers;
 
+import app.App;
+import app.models.RectangleModel;
+import app.utilities.Toolbelt;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,5 +21,42 @@ public class EncoderController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    public void handleMenuFileCloseButtonAction(ActionEvent event) throws Exception {
+        App.exitApplication();
+    }
+
+    @FXML
+    public void handleMenuHelpAboutButtonAction(ActionEvent event) throws Exception {
+        Toolbelt toolbelt = new Toolbelt();
+        Parent root = FXMLLoader.load(toolbelt.getResourceUrlAtPath("views/vwAbout.fxml"));
+
+        Stage stage = new Stage();
+        stage.setTitle ("About");
+        stage.setScene(new Scene(root, 550, 400));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+
+        RectangleModel bounds = toolbelt.getCenterForStageInWindow(stage.getScene().getWidth(), stage.getScene().getHeight());
+        stage.setX(bounds.getX());
+        stage.setY(bounds.getY());
+
+        stage.show();
+    }
+
+    @FXML
+    public void handleToolbarPlayButtonAction(ActionEvent event) throws Exception {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("This feature will be implemented soon!");
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void handleToolbarStopButtonAction(ActionEvent event) throws Exception {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("This feature will be implemented soon!");
+        alert.showAndWait();
     }
 }
