5e0b96bca3bce2a58c646b4a1c2d6c14a2bf6f44
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 21:44:13 -0400

    Added OpenPageConfirm

--- src/libraries/Pages.cs
+++ src/libraries/Pages.cs
@@ -7,4 +7,10 @@ public static class Pages
         using Form form = new T();
         return form.ShowDialog();
     }
+    
+    public static bool OpenPageConfirm<T>() where T : Form, new()
+    {
+        using Form form = new T();
+        return form.ShowDialog() is DialogResult.OK or DialogResult.Yes or DialogResult.Continue;
+    }
 }
