132bb2654ce033aab9148dec76e47345dfe1d4c3
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 23:33:08 -0400

    Added

---
+++ src/models/NotificationBar.cs
@@ -0,0 +1,23 @@
+﻿using Timer = System.Threading.Timer;
+
+namespace JackTheVideoRipper.models;
+
+public class NotificationBar
+{
+    public readonly Timer Timer;
+
+    public NotificationBar()
+    {
+        Timer = new Timer(ClearNotification, null, 0, 5000);
+    }
+
+    public void SetNotification(string notification)
+    {
+        Core.SendNotification(notification);
+    }
+
+    private void ClearNotification(object? state)
+    {
+        Core.SendNotification(string.Empty);
+    }
+}
