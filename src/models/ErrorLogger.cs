9fe77b9c45e2b52eef0d8bac7afb8e63446d4f9f
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 22:40:55 -0400

    Create ErrorLogger.cs

---
+++ src/models/ErrorLogger.cs
@@ -0,0 +1,47 @@
+﻿namespace JackTheVideoRipper.models;
+
+public class ErrorLogger
+{
+    private readonly Dictionary<string, List<ProcessError>> _errorTable = new();
+    
+    #region Logging Methods
+
+    private void InitializeLogging()
+    {
+        ProcessUpdateRow.ErrorLogEvent_Tag += LogError;
+        ProcessUpdateRow.ErrorLogEvent_Process += LogError;
+        ProcessUpdateRow.ErrorLogEvent_Error += LogError;
+    }
+
+    public void LogError(string processTag, Exception exception)
+    {
+        if (!_errorTable.ContainsKey(processTag))
+        {
+            _errorTable[processTag] = new List<ProcessError>();
+        }
+
+        _errorTable[processTag].Add(new ProcessError(processTag, exception));
+    }
+    
+    public void LogError(ProcessUpdateRow processUpdateRow, Exception exception)
+    {
+        if (!_errorTable.ContainsKey(processUpdateRow.Tag))
+        {
+            _errorTable[processUpdateRow.Tag] = new List<ProcessError>();
+        }
+
+        _errorTable[processUpdateRow.Tag].Add(new ProcessError(processUpdateRow, exception));
+    }
+    
+    public void LogError(ProcessError error)
+    {
+        if (!_errorTable.ContainsKey(error.ProcessTag))
+        {
+            _errorTable[error.ProcessTag] = new List<ProcessError>();
+        }
+
+        _errorTable[error.ProcessTag].Add(error);
+    }
+
+    #endregion
+}
