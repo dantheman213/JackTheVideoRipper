81134d5fa526f3efe33dd1482907eadbaf62f80f
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-04 11:53:46 -0400

    Added Tasks.WaitUntil

---
+++ src/libraries/Tasks.cs
@@ -0,0 +1,13 @@
+﻿namespace JackTheVideoRipper;
+
+public static class Tasks
+{
+    public static async Task WaitUntil(Func<bool> predicate, int delayInMilliseconds = 300)
+    {
+        while (!predicate())
+        {
+            Application.DoEvents();
+            await Task.Delay(delayInMilliseconds);
+        }
+    }
+}
