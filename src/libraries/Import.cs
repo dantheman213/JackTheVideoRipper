268b57cb8e5e7a313250f3c972923a97223e7442
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 21:46:36 -0400

    Update Import.cs

--- src/libraries/Import.cs
+++ src/libraries/Import.cs
@@ -4,12 +4,12 @@ namespace JackTheVideoRipper
 {
     internal static class Import
     {
-        private static readonly Regex urlPattern =
+        private static readonly Regex _UrlPattern =
             new(@"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?", RegexOptions.Compiled);
         
         public static IEnumerable<string> GetAllUrlsFromPayload(string s)
         {
-            return urlPattern.Matches(s).Select(i => i.Value);
+            return _UrlPattern.Matches(s).Select(i => i.Value);
         }
     }
 }
