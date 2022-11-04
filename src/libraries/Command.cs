ddf45ce13a0b084e5526eb46ea6098a8317b4f3a
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 21:34:10 -0400

    Added Command class

---
+++ src/libraries/Command.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace JackTheVideoRipper;
+
+public readonly struct Command
+{
+    private readonly string _executablePath;
+
+    public Command(string executablePath)
+    {
+        _executablePath = executablePath;
+    }
+
+    public string RunCommand(string parameters)
+    {
+        return FileSystem.RunCommand(_executablePath, parameters);
+    }
+    
+    public string RunWebCommand(string parameters, string url)
+    {
+        return FileSystem.RunCommand(_executablePath, $"{parameters} {url}");
+    }
+
+    public Process CreateCommand(string parameters)
+    {
+        return FileSystem.CreateProcess(_executablePath, parameters);
+    }
+}
