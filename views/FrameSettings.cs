0d7ecc8d55a971c22b9f897e394f68510c2831ad
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 23:28:58 -0400

    Update FrameSettings.cs

--- views/FrameSettings.cs
+++ views/FrameSettings.cs
@@ -2,39 +2,72 @@
 {
     public partial class FrameSettings : Form
     {
+        #region Form View Accessors
+
+        public int MaxConcurrentDownloads
+        {
+            get => (int) numMaxConcurrent.Value;
+            set => numMaxConcurrent.Value = value;
+        }
+
+        public string DefaultDownloadPath
+        {
+            get => textLocation.Text.Trim();
+            set => textLocation.Text = value;
+        }
+
+        #endregion
+
+        #region Constructor
+
         public FrameSettings()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Private Methods
+
+        private void GetSettings()
+        {
+            MaxConcurrentDownloads = Settings.Data.MaxConcurrentDownloads;
+            DefaultDownloadPath = Settings.Data.DefaultDownloadPath;
+        }
+
+        private void SetSettings()
+        {
+            Settings.Data.MaxConcurrentDownloads = MaxConcurrentDownloads;
+            Settings.Data.DefaultDownloadPath = DefaultDownloadPath;
+        }
+        
+        private static void Save()
+        {
+            Settings.Save();
+        }
+
+        #endregion
+
+        #region Form Events
+
         private void buttonLocationBrowse_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog folderBrowserDialog = new();
-            folderBrowserDialog.SelectedPath = textLocation.Text.Trim();
-            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-            {
-                textLocation.Text = folderBrowserDialog.SelectedPath;
-            }
+            if (FileSystem.SelectFile(textLocation.Text.Trim()) is { } selectedPath)
+                textLocation.Text = selectedPath;
         }
 
         private void FrameSettings_Load(object sender, EventArgs e)
         {
-            numMaxConcurrent.Value = Settings.Data.MaxConcurrentDownloads;
-            textLocation.Text = Settings.Data.DefaultDownloadPath;
+            GetSettings();
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
-            Settings.Data.MaxConcurrentDownloads = (int)numMaxConcurrent.Value;
-            Settings.Data.DefaultDownloadPath = textLocation.Text.Trim();
+            SetSettings();
             Save();
-
             Close();
         }
 
-        private void Save()
-        {
-            Settings.Save();
-        }
+        #endregion
     }
 }
