9d37fcd92c032d3a9dbe383717ca41ca01aa7ee7
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 22:19:22 -0400

    Moved DownloadMediaItem

---
+++ src/models/containers/DownloadMediaItem.cs
@@ -0,0 +1,24 @@
+﻿using JackTheVideoRipper.models;
+using JackTheVideoRipper.models.enums;
+using Newtonsoft.Json;
+
+namespace JackTheVideoRipper
+{
+    public class DownloadMediaItem
+    {
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+        
+        [JsonProperty("url")]
+        public string? Url { get; set; }
+        
+        [JsonProperty("parameters")]
+        public string? Parameters { get; set; }
+        
+        [JsonProperty("filepath")]
+        public string? Filepath { get; set; }
+        
+        [JsonProperty("media_type")]
+        public MediaType MediaType { get; set; }
+    }
+}
