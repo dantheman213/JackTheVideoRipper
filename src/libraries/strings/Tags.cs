b1cdc6bf2591d8f41010a21ea675b0c3864518fe
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 22:19:34 -0400

    Update Tags.cs

--- src/libraries/strings/Tags.cs
+++ src/libraries/strings/Tags.cs
@@ -6,14 +6,22 @@ public static class Tags
     public const string YOUTUBE = "[youtube]";
     public const string FFMPEG = "[ffmpeg]";
     public const string DOWNLOAD = "[download]";
+    public const string INFO = "[info]";
     
     public const string UNRECOGNIZED_CODEC = "unrecognized codec";
+    public const string DO_NOT_TRANSCODE = "(do not transcode)";
     public const string NONE = "none";
     public const string ERROR = "error";
+    
+    public const string LOW = "low";
+    public const string WORST_VIDEO = "worstvideo";
+    public const string BEST_VIDEO = "bestvideo";
+    public const string WORST_AUDIO = "worstaudio";
+    public const string BEST_AUDIO = "bestaudio";
+    
     public const string DASHES = "---";
     public const string PROGRESS_COMPLETE = "100%";
     public const string NOT_APPLICABLE = "N/A";
-    
     public const string DEFAULT_SPEED = "0.0 KB/s";
     public const string DEFAULT_PROGRESS = "0%";
     public const string DEFAULT_TIME = "--:--";
