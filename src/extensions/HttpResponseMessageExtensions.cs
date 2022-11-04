69cc5c5f14ec7b84ab28cebc3caf3d00100d0446
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-11-03 21:43:43 -0400

    Create HttpResponseMessageExtensions.cs

---
+++ src/extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace JackTheVideoRipper.extensions;
+
+public static class HttpResponseMessageExtensions
+{
+    public static string ResponseCode(this HttpResponseMessage response)
+    {
+        return $"{(int) response.StatusCode} ({response.ReasonPhrase})";
+    }
+
+    public static string DownloadResponse(this HttpResponseMessage response, string downloadPath)
+    {
+        try
+        {
+            using FileStream fileStream = new(downloadPath, FileMode.CreateNew);
+            response.Content.CopyToAsync(fileStream).Wait();
+        }
+        catch (Exception e)
+        {
+            throw new WebException("Failed to write web response to disk", e);
+        }
+
+        return downloadPath;
+    }
+
+    public static string GetResponse(this HttpResponseMessage response)
+    {
+        return response.Content.ReadAsStringAsync().Result;
+    }
+}
