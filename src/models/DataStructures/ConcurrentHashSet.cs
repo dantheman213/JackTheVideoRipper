56020252bd82cfdbe9954405ae15e08d3b4d3055
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-01-31 14:23:44 -0500

    Moved ConcurrentHashSet

---
+++ src/models/DataStructures/ConcurrentHashSet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using JackTheVideoRipper.extensions;
+
+namespace JackTheVideoRipper.models;
+
+public class ConcurrentHashSet<T> : IEnumerable<T> where T : notnull
+{
+    private readonly ConcurrentDictionary<T, byte> _data = new();
+
+    public void Add(T value)
+    {
+        _data.TryAdd(value, 0);
+    }
+
+    public bool Remove(T value)
+    {
+        return _data.TryRemove(new KeyValuePair<T, byte>(value, 0));
+    }
+    
+    public bool Remove(IEnumerable<T> values)
+    {
+        return values.Select(Remove).All();
+    }
+
+    public IEnumerable<T> Where(Func<T, bool> predicate)
+    {
+        return _data.Keys.Where(predicate);
+    }
+
+    public void Clear()
+    {
+        _data.Clear();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _data.Keys.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _data.Keys.GetEnumerator();
+    }
+}
