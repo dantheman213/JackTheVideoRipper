f778e3cf8458888217dd21b3b415c68652ff420a
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-01-31 14:25:40 -0500

    Moved IndexableQueue

---
+++ src/models/DataStructures/IndexableQueue.cs
@@ -0,0 +1,70 @@
+﻿namespace JackTheVideoRipper;
+
+public class IndexableQueue<T> : List<T>
+{
+    private readonly ReaderWriterLockSlim _queueLock = new();
+
+    private new int Count => base.Count;
+
+    public int Length
+    {
+        get
+        {
+            int length;
+            
+            _queueLock.EnterReadLock();
+            try
+            {
+                length = Count;
+            }
+            finally
+            {
+                _queueLock.ExitReadLock();
+            }
+            
+            return length;
+        }
+    }
+    
+    public T Dequeue()
+    {
+        T nextProcess = Peek();
+        Pop();
+        return nextProcess;
+    }
+
+    public T Peek()
+    {
+        T nextProcess;
+        
+        _queueLock.EnterReadLock();
+        try
+        {
+            nextProcess = this.First();
+        }
+        finally
+        {
+            _queueLock.ExitReadLock();
+        }
+
+        return nextProcess;
+    }
+
+    public void Pop()
+    {
+        _queueLock.EnterWriteLock();
+        try
+        {
+            RemoveAt(0);
+        }
+        finally
+        {
+            _queueLock.ExitWriteLock();
+        }
+    }
+
+    public bool Empty()
+    {
+        return Length == 0;
+    }
+}
