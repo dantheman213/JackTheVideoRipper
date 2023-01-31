b1614e70fad95fd419195476a840bd04599c0c68
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-01-31 14:10:32 -0500

    Basic Modals

---
+++ src/viewmodels/modals/BasicDropdownModal.cs
@@ -0,0 +1,19 @@
+﻿using JackTheVideoRipper.extensions;
+using JackTheVideoRipper.views.modals;
+
+namespace JackTheVideoRipper.viewmodels;
+
+public class BasicDropdownModal
+{
+    private readonly FrameBasicDropdown _frameBasicDropdown;
+    
+    public BasicDropdownModal(IEnumerable<string> options, string? title = null, string? defaultValue = null)
+    {
+        _frameBasicDropdown = new FrameBasicDropdown(options, title, defaultValue);
+    }
+    
+    public string? Open()
+    {
+        return _frameBasicDropdown.Confirm() ? _frameBasicDropdown.Value : null;
+    }
+}
---
+++ src/viewmodels/modals/BasicTextboxModal.cs
@@ -0,0 +1,19 @@
+﻿using JackTheVideoRipper.extensions;
+using JackTheVideoRipper.views.modals;
+
+namespace JackTheVideoRipper.viewmodels;
+
+public class BasicTextboxModal
+{
+    private readonly FrameBasicTextbox _frameBasicTextbox;
+    
+    public BasicTextboxModal(string? title = null, string? defaultValue = null)
+    {
+        _frameBasicTextbox = new FrameBasicTextbox(title, defaultValue);
+    }
+
+    public string? Open()
+    {
+        return _frameBasicTextbox.Confirm() ? _frameBasicTextbox.Value : null;
+    }
+}
